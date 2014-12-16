17e0b8cbc65f19b080bc5557f842043141b0dc72
Matthew Grasinger <grasingerm@gmail.com>
2014-12-16 13:10:33 -0500

    commit template:
    Mod: added commit template to default git configuration

--- git-default-config.sh
+++ git-default-config.sh
@@ -3,3 +3,6 @@
 git config --global user.name "Matthew Grasinger"
 git config --global user.email "grasingerm@gmail.com"
 git config --global push.default=matching
+
+cp .gitmessage.txt ~/
+git config --global commit.template ~/.gitmessage.txt
