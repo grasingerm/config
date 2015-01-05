30a7e97e500b28c2a41ae7c1fec9f048b6adbffa
Matthew Grasinger <grasingerm@gmail.com>
2015-01-05 11:20:51 -0500

    Fix: invalid key error

--- git-default-config.sh
+++ git-default-config.sh
@@ -2,7 +2,7 @@
 
 git config --global user.name "Matthew Grasinger"
 git config --global user.email "grasingerm@gmail.com"
-git config --global push.default=matching
+git config --global push.default matching
 git config --global core.editor vim
 
 cp .gitmessage.txt ~/
