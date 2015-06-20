cd62ae54dfa4c314c69e65a79fb83babb764c1db
Matthew Grasinger <grasingerm@gmail.com>
2015-06-20 02:53:53 -0600

    fixing powershell init script

--- init.ps1
+++ init.ps1
@@ -1,5 +1,5 @@
-cmd.exe /c mklink /H .\vim\.vimrc ~\.vimrc
-cmd.exe /c mklink /H .\git\.gitconfig ~\.gitconfig
-cmd.exe /c mklink /H .\git\.gitmessage.txt ~\.gitmessage.txt
+cmd.exe /c mklink /H C:\Users\Matthew\.vimrc .\vim\.vimrc
+cmd.exe /c mklink /H C:\Users\Matthew\.gitconfig .\git\.gitconfig
+cmd.exe /c mklink /H C:\Users\Matthew\.gitmessage.txt .\git\.gitmessage.txt
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
